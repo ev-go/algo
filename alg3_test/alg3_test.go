10c57a46ea67a96ae16b42d70d59c846b8b86f63
Dmitry T <dmitry_dtv@mail.ru>
2023-06-21 17:59:35 +0500

    wip

--- alg3_test/alg3_test.go
+++ alg3_test/alg3_test.go
@@ -1,13 +1,47 @@
 package alg3_test
 
-//есть грядка и кол-во растений которые надо посадить, но садить можно с условием только чтобы по бокам было свободно, надо вернуть тру или фолз можно ли посадить все растения или нет, не изменяя грядку, занятое место = 1, свободное = 0
-//пример
-//[1, 0, 0, 0, 1], k = 1
-//true // [1, 0, 1, 0, 1]
-//
-//[0, 0, 1, 0], k = 1
-//true // [1, 0, 1, 0]
-//
-//[0, 1, 0, 0, 1, 0], k = 2
-//false
-//ну крче за O(1) по памяти над и по времени O(n)
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSubstring(t *testing.T) {
+	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	k := 0
+	j := 0
+	max := 0
+	curr := 0
+	fmt.Println(s[0:0])
+	for range s[k : j+1] {
+		if k == j {
+			if 1 >= max {
+				max = 1
+				curr = 1
+			}
+			continue
+		}
+
+		for i := k; i < j; i++ {
+			if s[i] == s[j] {
+				j += i + 1
+
+				if curr >= max {
+					max = curr
+				}
+				curr = curr - (i + 1)
+				break
+			}
+
+			curr++
+
+		}
+		if curr >= max {
+			max = curr
+		}
+	}
+
+	return max
+}
